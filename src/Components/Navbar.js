06c5efc273416f03d1d28326552274a8bdb4aa0f
dhanisetiaji <dhanisetiaji99@gmail.com>
2022-07-03 18:07:08 +0700

    add classname

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -30,7 +30,7 @@ export const Navbar = () => {
               </li>
             </ul>
             <form className="d-flex">
-              <button className="btn btn-primary" id="sign-up" type="submit"><Link to={'/admin'}>Sign Up</Link></button>
+              <button className="btn btn-primary" id="sign-up" type="submit"><Link to={'/admin'} className="text-decoration-none text-white">Sign Up</Link></button>
             </form>
           </div>
         </div>
